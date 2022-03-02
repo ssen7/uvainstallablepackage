ee3ba9f7abfadeb0383902a5a82fd4904ab01a41
Niarfe <efrain.olivares@gmail.com>
2022-03-02 02:21:07 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='shared',
-      version='0.0.2',
+      version='0.0.3',
       description='This package has shared components.',
       author='Efrain Olivares',
       author_email='efrain.olivares@gmail.com',
