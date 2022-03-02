6de84306e3d44d094d9290f134892ea850c7574f
Niarfe <efrain.olivares@gmail.com>
2022-03-01 21:23:38 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='shared',
+      version='0.0.1',
+      description='This package has shared components.',
+      author='Efrain Olivares',
+      author_email='efrain.olivares@gmail.com',
+      packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+      license='LICENSE.txt',
+    )
