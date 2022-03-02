e7932b51fd913fab744fa12ec0e91a90f6d6eb00
Niarfe <efrain.olivares@gmail.com>
2022-03-02 02:09:06 -0800

    Update shared.py

--- shared/shared.py
+++ shared/shared.py
@@ -1,2 +1,13 @@
 def afunction():
     print("This is the installed function")
+
+def clean_string(str_word):
+    """
+    INPUTS:
+    str_word     string string contining several words to clean
+    RETURNS:
+    string       the string after removal of extra spaces, punctuation and lowercasing
+    """
+    assert isinstance(str_word, str), "String expected but recieved type {} instead".format(type(str_word))
+
+    return str_word
