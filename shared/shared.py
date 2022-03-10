521a42f5a3325ca674f792bfe45e4cf6391df85e
Niarfe <efrain.olivares@gmail.com>
2022-03-09 17:22:28 -0800

    Update shared.py

--- shared/shared.py
+++ shared/shared.py
@@ -16,4 +16,4 @@ import re
 def space_compress(stocomp):
     assert isinstance(stocomp, str), "Expected str but got {} instead".format(type(stocomp))
     comp = re.sub(r'\s+', ' ', stocomp)
-    return comp
+    return comp.strip()
