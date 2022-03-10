533ca0fdf957e62ee4c025d1113982f29f3fbce6
Niarfe <efrain.olivares@gmail.com>
2022-03-09 17:49:41 -0800

    Update testit.py

--- testit.py
+++ testit.py
@@ -4,6 +4,7 @@ sh.afunction()
 def test_multi_spaces():
     assert sh.space_compress('word       word     word') == 'word word word', "Failed multi space"
 
+
 def test_multi_lines():
     multiline = """
     word
@@ -11,3 +12,19 @@ def test_multi_lines():
     word
     """
     assert sh.space_compress(multiline) == 'word word word', "Failed multi line"
+
+
+def test_assert_int():
+    with pytest.raises(Exception) as exc_mssg:
+        sh.space_compress(2)
+
+    assert "Expected str but got" in str(exc_mssg)
+    assert "int" in str(exc_mssg)
+
+
+def test_assert_bool():
+    with pytest.raises(Exception) as exc_mssg:
+        sh.space_compress(False)
+
+    assert "Expected str but got" in str(exc_mssg)
+    assert "bool" in str(exc_mssg)
