8634580847811230dcb3b0129553087595f3b318
Niarfe <efrain.olivares@gmail.com>
2022-03-09 16:57:51 -0800

    Update shared.py

--- shared/shared.py
+++ shared/shared.py
@@ -11,3 +11,9 @@ def clean_string(str_word):
     assert isinstance(str_word, str), "String expected but recieved type {} instead".format(type(str_word))
 
     return str_word
+
+import re
+def space_compress(stocomp):
+    assert isinstance(stocomp, str), "Expected str but got {} instead".format(type(stocomp))
+    comp = re.sub(r'\s+', ' ', stocomp)
+    return comp
