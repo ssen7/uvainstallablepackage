d6c9b60bff1ea311ba8d673034c714275f735c07
EfrainOlivaresEv <eolivares@evidation.com>
2022-05-06 11:27:21 -0700

    setting up for circle ci

--- shared/__init__.py
+++ shared/__init__.py
@@ -1,1 +1,1 @@
-from shared.shared import afunction, clean_string, space_compress
+from shared.shared import afunction, clean_string, space_compress, Singleton
---
+++ tests/test_shared.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append('.')
+from shared import Singleton
+
+def afunction():
+    print("This is the installed function")
+
+def clean_string(str_word):
+    """
+    INPUTS:
+    str_word     string string contining several words to clean
+    RETURNS:
+    string       the string after removal of extra spaces, punctuation and lowercasing
+    """
+    assert isinstance(str_word, str), "String expected but recieved type {} instead".format(type(str_word))
+
+    return str_word
+
+import re
+def space_compress(stocomp):
+    assert isinstance(stocomp, str), "Expected str but got {} instead".format(type(stocomp))
+    comp = re.sub(r'\s+', ' ', stocomp)
+    return comp.strip()
+
+import sqlite3
+import os
+
+
+
+def test_is_singleton():
+    a = Singleton()
+    b = Singleton()
+
+    assert id(a) == id(b)
+
